c5fa4e14bf8f5b93602dc78324dfa23f655dfbc3
osimjon <osimjon90@yandex.ru>
2024-03-07 17:22:18 +0300

    add swiper and create <section activities

--- js/script.js
+++ js/script.js
@@ -8,29 +8,62 @@ burger.addEventListener("click", () => {
   openMenu.classList.toggle("open-menu");
   headerUser.classList.toggle("open-menu");
 });
-document.addEventListener("click", (e) => {
-  const targetElement = e.target;
-  console.log("targetElement: ", targetElement);
-  if (openMenu && openMenu.classList.contains("open-menu")) {
-    console.log("targetElement: ", targetElement);
-  }
-});
 
+// Slider
+/*
+const sliderLine = document.querySelector(".slider__line");
+const slide = document.querySelectorAll(".slider__slide");
+
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
+
+let counter = 0;
+let slideWidth = slide[0].clientWidth;
+sliderLine.style.width = 3 * slideWidth   + "px";
+console.log('sliderLine: ', sliderLine);
+
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+function nextSlide() {
+  if (counter >= slide.length - 1) {
+    counter = -1;
+  }
+  counter++;
+  console.log("counter: ", counter);
+  line();
+}
+function prevSlide() {
+  if (counter <= 0) {
+    counter = 1;
+  }
+  counter--;
+  console.log("counter: ", counter);
+  line();
+}
+
+function line() {
+  sliderLine.style.transform = `translateX(${-counter * slideWidth}px)`;
+}
+*/
 const swiper = new Swiper(".swiper", {
   // Optional parameters
+  direction: "horizontal",
   loop: true,
   autoplay: {
     delay: 5000,
   },
+  speed: 1000,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+    nextEl: ".swiper-button_next",
+    prevEl: ".swiper-button_prev",
   },
 });
